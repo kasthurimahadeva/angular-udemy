1ec71fd482789a8f3728cac1786f8fb964a435de
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-12 04:23:04 +0530

    selector as html-element

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
-  // tslint:disable-next-line:component-selector
-  selector: '.app-servers',
+  // selector: '.app-servers',
+  selector: 'app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>
