b6e93f70a253b38fbc7faa89f10461525dc69b1d
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-12 04:21:56 +0530

    selector as class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
   // tslint:disable-next-line:component-selector
-  selector: '[app-servers]',
+  selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>
