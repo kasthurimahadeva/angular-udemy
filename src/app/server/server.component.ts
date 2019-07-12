b883063ca365ab37cdfe0b530b8d3efd216bfb6c
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-12 09:19:23 +0530

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,17 @@ import {Component} from '@angular/core';
 
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = 'offline';
+  serverStatus: string;
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus(): string {
     return this.serverStatus;
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
