bd35bd564df957d9dfdcb46a565c9012999ffed7
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-29 23:53:18 +0530

    clean the codes for course project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
   ],
   imports: [
     BrowserModule,
